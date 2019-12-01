e214f331984094bebe4022d7e50b779f7fc12826
Paul <dev.pauljoo@gmail.com>
2019-12-01 16:06:36 +1100

    Initial Commit.

---
+++ DigIO-Programming-Task-API/Controllers/LogController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigIO_Programming_Task_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class LogController : Controller
+    {
+        [HttpPost]
+        public ActionResult<IEnumerable<string>> ParseLog(IFormFile file)
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ DigIO-Programming-Task-Unit-Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DigIO_Programming_Task_Unit_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
