636f118f8d6f3e0c65d4fd032b842fcdfe82d01a
Paul <dev.pauljoo@gmail.com>
2019-12-06 21:32:09 +1100

    Log Activity Analysis.

--- DigIO-Programming-Task-API/Models/ParseLogResponse.cs
+++ DigIO-Programming-Task-API/Models/ParseLogResponse.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using DigIO_Programming_Task_Services.Models;
+using System.Collections.Generic;
 
 namespace DigIO_Programming_Task_API.Models
 {
     public class ParseLogResponse
     {
+        public LogAnalysis LogAnalysis { get; set; }
         public IEnumerable<string> Errors { get; set; }
     }
 }
---
+++ DigIO-Programming-Task-Services/Models/LogAnalysis.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DigIO_Programming_Task_Services.Models
+{
+    public class LogAnalysis
+    {
+        public int NumberOfUniqueIpAddresses { get; set; }
+        public IEnumerable<RecordAndCount> TopThreeVisitedUrls { get; set; }
+        public IEnumerable<RecordAndCount> TopThreeActiveIpAddresses { get; set; }
+    }
+}
